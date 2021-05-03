79f3b4b1418aa227dc803a5a769b118d0113638c
kurwitz3 <kurwitz3@aol.com>
2021-05-03 17:27:58 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -6,24 +6,23 @@ import Home from './components/Home'
 import Portfolio from './components/Portfolio'
 import Resume from './components/Resume'
 import About from './components/About'
-import Contact from './components/Contact'
+
 
 function App() {
   return (
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-          <Switch>
-             <Route exact path='/' component={Home}/>
-             <Route path='/Portfolio' component={Portfolio}/>
-             <Route path='/Resume' component={Resume}/>
-             <Route path='/About' component={About}/>
-             <Route path='/Contact' component={Contact}/>
-          </Switch>
-      </header>
-    </div>
-  </Router>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+            <Switch>
+               <Route exact path='/' component={Home}/>
+               <Route path='/Portfolio' component={Portfolio}/>
+               <Route path='/Resume' component={Resume}/>
+               <Route path='/About' component={About}/>
+            </Switch>
+        </header>
+      </div>
+    </Router>
   );
 }
 
