136179cc45172ee7f198ee3343a813d817772a69
kurwitz3 <kurwitz3@aol.com>
2021-07-14 17:53:59 -0400

    made the portfolio css file

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../CSS/Portfolio.css'
 import ILoveIpa from '../projects/ILoveIpa'
 import AmericanOutfitters from '../projects/AmericanOutfitters'
 import { Component } from 'react'
