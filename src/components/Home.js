94cea57fa8e81ac642cae9b2940621e1741b40bf
kurwitz3 <kurwitz3@aol.com>
2021-07-18 20:39:09 -0400

    finished homepage css

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,16 @@ import '../CSS/HomePage.css'
 
 const Home = () =>{
     return (
-        <div id= 'home-div'>
-          <p id="welcome">Hello! I'm</p>
-          <h1 id="name">Richard Kurwitz</h1>
-          <p id="title">I'm a FlatIron School graduate who has experience in Ruby on Rails and JavaScript based programming. 
+        <div id= 'homePage__container'>
+          <p id="homePage__welcome">Hello! I'm</p>
+            <h1 id="homePage__name">Richard Kurwitz</h1>
+          <p id="homePage__opening-paragraph">I'm a FlatIron School graduate who has experience in Ruby on Rails and JavaScript based programming. 
           A former certified welder making a career change and bringing the same dedication and strong work ethics to job performance and appyling it to coding. 
           With relentless positivity and strong teamwork skills, I will make the perfect candidate to work in any programming environment.
           </p>
-          <img id='me' src={Pic} alt=''></img>
+            <div className='homePage__pic-container'>
+               <img id='homePage__pic' src={Pic} alt=''></img>
+            </div>
         </div>
     )
 }
