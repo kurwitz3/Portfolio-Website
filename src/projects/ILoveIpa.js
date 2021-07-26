a2aa03d9671f1e74f5868b17c679fdc413110ed0
kurwitz3 <kurwitz3@aol.com>
2021-07-25 23:04:42 -0400

    html changes

--- src/projects/ILoveIpa.js
+++ src/projects/ILoveIpa.js
@@ -6,20 +6,16 @@ const ILoveIpa = () => {
       <div >
          <div className="portfolio__pic">
             <h2 className='portfolio__h2'>I Love IPA</h2>
-            <div className="portfolio__project-links">
-            <a className='portfolio__demo' href='https://wonderful-lamarr-3ee816.netlify.app'>DEMO</a>
-            <a className='portfolio__Frontend' href='https://github.com/kurwitz3/I-Love-Ipa-Frontend'>FRONT END</a>
-            <a className='portfolio__Backend' href='https://github.com/kurwitz3/I-Love-Ipa-Backend'>BACK END</a>
-            </div>
+              <div className="portfolio__project-links">
+                 <a className='portfolio__demo' href='https://wonderful-lamarr-3ee816.netlify.app'>DEMO</a>
+                 <a className='portfolio__Frontend' href='https://github.com/kurwitz3/I-Love-Ipa-Frontend'>FRONT END</a>
+                 <a className='portfolio__Backend' href='https://github.com/kurwitz3/I-Love-Ipa-Backend'>BACK END</a>
+              </div>
             <img className='portfolio__image' src={Pic} alt=''></img>
-            
           </div>
-         <div className="portfolio__buttons">
-         
-         </div>
-         
       </div>
     )
 }
+
 export default ILoveIpa
 
